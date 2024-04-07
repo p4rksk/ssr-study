5f49738594179975d2f5a98e441e9995421a0be2
yeeun0118s <yeeun0118s@naver.com>
2024-04-07 18:53:15 +0900

    댓글 삭제

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -4,12 +4,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import shop.mtcoding.blog._core.errors.exception.Exception403;
-import shop.mtcoding.blog._core.errors.exception.Exception404;
 import shop.mtcoding.blog.user.User;
 
 import java.util.List;
@@ -18,33 +15,26 @@ import java.util.List;
 @Controller // new BoardController(IoC에서 BoardRepository를 찾아서 주입) -> IoC 컨테이너 등록
 public class BoardController {
 
-    private final BoardRepository boardRepository;
+    private final BoardService boardService;
     private final HttpSession session;
 
     @PostMapping("/board/save")
     public String save(BoardRequest.SaveDTO reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        boardRepository.save(reqDTO.toEntity(sessionUser));
+        boardService.글쓰기(reqDTO, sessionUser);
         return "redirect:/";
     }
 
     @PostMapping("/board/{id}/update")
     public String update(@PathVariable Integer id, BoardRequest.UpdateDTO reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        Board board = boardRepository.findById(id);
-
-        if(sessionUser.getId() != board.getUser().getId()){
-            throw new Exception403("게시글을 수정할 권한이 없습니다");
-        }
-
-        boardRepository.updateById(id, reqDTO.getTitle(), reqDTO.getContent());
+        boardService.글수정(id, sessionUser.getId(), reqDTO);
         return "redirect:/board/" + id;
     }
 
     @GetMapping("/board/{id}/update-form")
     public String updateForm(@PathVariable Integer id, HttpServletRequest request) {
-        Board board = boardRepository.findById(id);
-
+        Board board = boardService.글조회(id);
         request.setAttribute("board", board);
         return "board/update-form";
     }
@@ -52,19 +42,13 @@ public class BoardController {
     @PostMapping("/board/{id}/delete")
     public String delete(@PathVariable Integer id) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        Board board = boardRepository.findById(id);
-
-        if(sessionUser.getId() != board.getUser().getId()){
-            throw new Exception403("게시글을 삭제할 권한이 없습니다");
-        }
-
-        boardRepository.deleteById(id);
+        boardService.글삭제(id, sessionUser.getId());
         return "redirect:/";
     }
 
     @GetMapping("/")
     public String index(HttpServletRequest request) {
-        List<Board> boardList = boardRepository.findAll();
+        List<Board> boardList = boardService.글목록조회();
         request.setAttribute("boardList", boardList);
         return "index";
     }
@@ -74,21 +58,14 @@ public class BoardController {
         return "board/save-form";
     }
 
+    // SSR은 DTO를 굳이 만들필요가 없다. 필요한 데이터만 랜더링해서 클라이언트에게 전달할것이니까!!
     @GetMapping("/board/{id}")
     public String detail(@PathVariable Integer id, HttpServletRequest request) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        Board board = boardRepository.findByIdJoinUser(id);
+        Board board = boardService.글상세보기(id, sessionUser);
 
-        // 로그인을 하고, 게시글의 주인이면 isOwner가 true가 된다.
-        boolean isOwner = false;
-        if(sessionUser != null){
-            if(sessionUser.getId() == board.getUser().getId()){
-                isOwner = true;
-            }
-        }
-
-        request.setAttribute("isOwner", isOwner);
         request.setAttribute("board", board);
         return "board/detail";
     }
+
 }
--- src/main/java/shop/mtcoding/blog/reply/ReplyController.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyController.java
@@ -3,6 +3,7 @@ package shop.mtcoding.blog.reply;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import shop.mtcoding.blog.user.User;
 
@@ -17,5 +18,12 @@ public class ReplyController {
         User sessionUser = (User) session.getAttribute("sessionUser");
         replyService.댓글쓰기(reqDTO, sessionUser);
         return "redirect:/board/"+reqDTO.getBoardId();
+    }
+
+    @PostMapping("/board/{boardId}/reply/{replyId}/delete")
+    public String delete(@PathVariable Integer boardId, @PathVariable Integer replyId){ //컨트롤러에 책임은 인증체크 나머지는 서비스로
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        replyService.댓글삭제(replyId, sessionUser.getId());
+        return "redirect:/board/"+boardId;
     }
 }
